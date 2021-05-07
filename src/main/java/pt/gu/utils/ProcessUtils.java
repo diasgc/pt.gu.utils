ca863035609759a508c0b67f92cc9365d37091b9
gusta <gustavocdias@me.com>
2021-05-07 12:16:25 +0100

    upd

--- src/main/java/pt/gu/utils/ProcessUtils.java
+++ src/main/java/pt/gu/utils/ProcessUtils.java
@@ -2,22 +2,22 @@ package pt.gu.utils;
 
 import android.content.Context;
 import android.net.Uri;
-import android.os.Build;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ProcessUtils {
 
+    public static final String LD_LIBRARY_PATH = "LD_LIBRARY_PATH";
+
     public static class Builder {
 
-        Object is;
-        Object os;
+        Object in;
+        Object out;
 
         ProcessBuilder builder;
         List<String> args = new ArrayList<>();
@@ -27,33 +27,48 @@ public class ProcessUtils {
             builder = new ProcessBuilder();
         }
 
-        public Builder redirectInput(File file) throws FileNotFoundException {
-            is = file;
+        public Builder cmd(String... cmds){
+            if (cmds != null)
+                args.addAll(Arrays.asList(cmds));
             return this;
         }
 
-        public Builder redirectInput(InputStream is){
-            this.is = is;
+        public Builder input(File file) {
+            in = file;
             return this;
         }
 
-        public Builder redirectInput(Context context, Uri uri) throws FileNotFoundException {
-            if ("content".equals(uri.getScheme()))
-                this.is = context.getContentResolver().openInputStream(uri);
-            else if (StringUtils.equalsAny(uri.getScheme(),"http","https","ftp")){
-                this.is = IoUtils.HttpInputStream.openUrl(uri);
-            }
+        public Builder input(InputStream is){
+            this.in = is;
+            return this;
+        }
+
+        public Builder input(Uri uri) {
+            this.in = uri;
+            return this;
+        }
+
+        public Builder output(File file) {
+            this.out = file;
+            return this;
+        }
+
+        public Builder output(InputStream is){
+            this.out = is;
+            return this;
+        }
+
+        public Builder output(Uri uri) {
+            this.out = uri;
             return this;
         }
 
         public Builder setLdPath(String ldPath){
-            builder.environment().put("LD_LIBRARY_PATH",ldPath);
-            return this;
+            return setEnv(LD_LIBRARY_PATH,ldPath);
         }
 
         public Builder setLdPath(Context context){
-            builder.environment().put("LD_LIBRARY_PATH",context.getApplicationInfo().nativeLibraryDir);
-            return this;
+            return setEnv(LD_LIBRARY_PATH,context.getApplicationInfo().nativeLibraryDir);
         }
 
         public Builder setEnv(String var, String val){
